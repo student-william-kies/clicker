6f7777c411a82b3e3d98c6fac98ba5180e92ce04
William² <william.kies@laplateforme.io>
2021-04-23 11:48:20 +0200

    commit for merge

---
+++ js/script.js
@@ -0,0 +1,34 @@
+let nbGold = 0;
+
+/**
+ * Variables des items
+ */
+let lvl = 1;
+let lvlShield = 0;
+let lvlLunette = 0;
+
+/**
+ * Variables des spells
+ */
+let lvlHourGlass = 0;
+
+/**
+ * Variables des monstres
+ */
+let x = 0;
+let nbClick = 0;
+
+/**
+ * Enregistre le nombre clics
+ */
+function mouseClicked()
+{
+    nbGold += lvl;
+    nbGold += lvlShield;
+    nbGold += lvlLunette;
+    nbGold += nbClick;
+    x += lvl;
+    earningCoin();
+    heroAnimation1();
+    popMonster();
+}
